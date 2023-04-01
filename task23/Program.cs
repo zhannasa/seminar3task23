680dc7ef453f3824c2724357b3a96a8e85183e6f
Zhanna <zhannass@gmai.com>
2023-04-01 21:47:52 +0200

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Introduce un numero: ");
+int number =int.Parse(Console.ReadLine());
+//int i = 1;
+
+//while (i <= number)
+//{
+  // Console.WriteLine($"{i*i*i}");
+   //i++;
+//}
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+}
